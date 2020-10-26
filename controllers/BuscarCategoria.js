baf67aae38ea2d7af11af37e83af6cd8369cc7d7
radames vaz <rgvtovar@hotmail.com>
2020-10-26 11:26:44 -0400

    modificando endpoint busqueda categorias

--- controllers/BuscarCategoria.js
+++ controllers/BuscarCategoria.js
@@ -1,9 +1,7 @@
 const handleBuscarCategoria = (req, res, db) => {
     const { categoria } = req.body;
-    db.select('*').from('productos').where({
-        categoria : categoria
-    }).then(user => {
-        if(user.length){
+    db.select('*').from('productos').where({ categoria }).then(user => {
+        if(user.length > 0){
             res.json(user)
         }else{
             res.status(400).json('productos no encontrados')
