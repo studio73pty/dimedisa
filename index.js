af17324d8f6bcf548f11ddba92b34ca0d2282221
radames vaz <rgvtovar@hotmail.com>
2020-11-05 18:02:48 -0400

    agregando paypal

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 require('dotenv').config();
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
+const paypal = require('paypal-rest-sdk');
 
 
 
@@ -35,6 +36,12 @@ const db = knex({
       database: process.env.DATABASE
     }
   });
+
+paypal.configure({
+  'mode': 'sandbox',
+  'client_id': process.env.CLIENT_ID,
+  'client_secret': process.env.CLIENT_SECRET
+})
 
 const app = express();
 
@@ -163,9 +170,99 @@ app.use('/imagen-producto/:id', upload.array('image'), async(req, res) => {
   
 })
 
-//Borrar Imagen de producto
 
 
+//  Endpoints de paypal
+app.post('/pay', async (req, res) =>{
+    const reqQuery = { ...req.query };
+   
+   let queryProd = reqQuery.id.split(',');
+   let queryCant = reqQuery.cantidad.split(',');
+   
+   let productosArr = [];
+   let cantidadArr = [];
+
+   for(let i = 0; i < queryProd.length; i++){
+     let producto = await db('productos').select().where({ id: queryProd[i] });
+     productosArr.push(producto);
+   }
+
+   for(let i = 0; i < queryCant.length; i++){
+     cantidadArr.push(queryCant[i]);
+   }
+   
+   res.json(cantidadArr)
+
+
+
+/*
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+        "payment_method": "paypal"
+    },
+    "redirect_urls": {
+        "return_url": "http://localhost:3000/success",
+        "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+        "item_list": {
+            "items": [{
+                "name": "Iphone X",
+                "sku": "001",
+                "price": "25.00",
+                "currency": "USD",
+                "quantity": 1
+            }]
+        },
+        "amount": {
+            "currency": "USD",
+            "total": "25.00"
+        },
+        "description": "Iphone X prueba cliente"
+    }]
+};
+
+paypal.payment.create(create_payment_json, function (error, payment) {
+  if (error) {
+      throw error;
+  } else {
+      for(let i = 0; i < payment.links.length; i++){
+          if(payment.links[i].rel == 'approval_url'){
+              res.send(payment.links[i].href)
+          }
+      }
+  }
+});
+*/
+})
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+
+  const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log(JSON.stringify(payment));
+          res.send('Success');
+      }
+  });
+  });
+
+app.get('/cancel', (req, res) => res.send('Cancelled'));
 
 
 const port = process.env.PORT || 3000;
